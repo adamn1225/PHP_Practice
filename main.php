48246f3f37b71f49ed5549cf241b285f714e824c
Adam <Anoah1225@gmail.com>
2018-12-11 13:09:34 -0500

    first commit

---
+++ main.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Practice<title>
+  </head>
+  <body>
+
+    <?php
+    echo 'Beginning my PHP journey';
+
+    $x = 1;
+    $y = 2;
+    $sum = $x + $y;
+
+    echo $sum;
+    ?>
+
+  </body>
+</html>
